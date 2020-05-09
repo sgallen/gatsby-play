7ba6f4871939be34e454060b5bc48f60a20053fe
Scott Allen <sgallen@gmail.com>
2020-05-09 14:33:14 -0400

    Create simple first set of pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,25 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  const value = "first"
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <h2>This is a {value} look at Gatsby.</h2>
+      <ul>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default IndexPage
