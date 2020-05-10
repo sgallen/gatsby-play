bf73978990815361baad74656d279d89067eb340
Scott Allen <sgallen@gmail.com>
2020-05-10 12:43:23 -0400

    Introduce a basic css module for the About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import React from "react"
 
 import { Layout } from "../components"
+import AboutStyle from "./about.module.scss"
 
 const AboutPage = () => {
   return (
     <Layout>
-      <h1>About</h1>
+      <h1 className={AboutStyle.dodger}>About page</h1>
     </Layout>
   )
 }
