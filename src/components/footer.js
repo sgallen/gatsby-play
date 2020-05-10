7ec27dea0ee65f0e3c7dac3a1d19dd66a0ab7b99
Scott Allen <sgallen@gmail.com>
2020-05-09 20:00:16 -0400

    Make footer year dynamic

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
   return (
     <footer>
-      <p>Created by sga, © 2020</p>
+      <p>Created by sga, © {new Date().getFullYear()}</p>
     </footer>
   )
 }
